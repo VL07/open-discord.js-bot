0638e85aba9bd4431887c1d88014935d5e4d4814
VL07 <79533745+VL07@users.noreply.github.com>
2021-12-28 18:51:14 +0100

    Added timeout remove command

---
+++ src/commands/administration/timeout/remove.js
@@ -0,0 +1,63 @@
+const { SlashCommandSubcommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandSubcommandBuilder()
+		.setName("remove")
+		.setDescription("Timeout a member")
+		.addUserOption(option =>
+			option
+				.setName("member")
+				.setDescription("The user to ban")
+				.setRequired(true),
+		),
+	permissions: ["TIMEOUT_MEMBERS"],
+	async execute(interaction) {
+		const user = interaction.options.getMember("member");
+		if (!user.communicationDisabledUntilTimestamp || user.communicationDisabledUntilTimestamp < Date.now()) {
+			const embed = new MessageEmbed()
+				.setColor("2F3136")
+				.setTitle("❌ | Error")
+				.setDescription(`<@${user.id}> is not timed out`)
+				.setTimestamp()
+				.setFooter("Use /help to get help");
+
+			await interaction.reply({
+				embeds: [embed],
+				ephemeral: true,
+			});
+
+			return;
+		}
+
+		try {
+			await user.disableCommunicationUntil(Date.now(), "Removed timeout");
+		} catch (err) {
+			console.log(err);
+			const embed = new MessageEmbed()
+				.setColor("2F3136")
+				.setTitle("❌ | Error")
+				.setDescription(`Couldn't remove the timeout from <@${user.id}>`)
+				.setTimestamp()
+				.setFooter("Use /help to get help");
+
+			interaction.reply({
+				embeds: [embed],
+				ephemeral: true,
+			});
+			return;
+		}
+
+		const embed = new MessageEmbed()
+			.setColor("2F3136")
+			.setTitle("⏳ | Successfully Timed Out")
+			.setDescription(`Successfully removed the time out from <@${user.id}>`)
+			.setTimestamp()
+			.setFooter("Use /help to get help");
+
+		interaction.reply({
+			embeds: [embed],
+			ephemeral: true,
+		});
+	},
+};
